49d7ce7fd8f59739e8f42bf89481293961eee195
Viktor Gamov <viktor@confluent.io>
2020-09-24 00:43:28 -0400

    Refactored chapter 3

--- KafkaInAction_Chapter3/src/main/java/com/kafkainaction/consumer/HelloWorldConsumer.java
+++ KafkaInAction_Chapter3/src/main/java/com/kafkainaction/consumer/HelloWorldConsumer.java
@@ -1,37 +1,58 @@
 package com.kafkainaction.consumer;
 
-import java.util.Arrays;
-import java.util.Properties;
+import com.kafkainaction.Alert;
+import com.kafkainaction.producer.HelloWorldProducer;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.kafkainaction.Alert;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 public class HelloWorldConsumer {
 
-	public static void main(String[] args) {
-		Properties props = new Properties();
-		props.put("bootstrap.servers", "localhost:9092,localhost:9093");
-		props.put("group.id", "helloconsumer");
-		props.put("enable.auto.commit", "true");
-		props.put("auto.commit.interval.ms", "1000");
-		props.put("key.deserializer", "org.apache.kafka.common.serialization.LongDeserializer");
-		props.put("value.deserializer", "io.confluent.kafka.serializers.KafkaAvroDeserializer");
-		props.put("schema.registry.url", "http://localhost:8081");
+  final static Logger log = LoggerFactory.getLogger(HelloWorldProducer.class);
 
-		@SuppressWarnings("resource")
-		KafkaConsumer<Long, Alert> consumer = new KafkaConsumer<Long, Alert>(props);
-		consumer.subscribe(Arrays.asList("avrotest"));
+  private volatile boolean keepConsuming = true;
 
-		while (true) {
-			ConsumerRecords<Long, Alert> records = consumer.poll(100);
-			for (ConsumerRecord<Long, Alert> record : records)
-				System.out.printf("offset = %d, key = %d, value = %s%n", record.offset(), record.key(), record.value());
-		}
+  public static void main(String[] args) {
+    Properties props = new Properties();
+    props.put("bootstrap.servers", "localhost:9092");
+    props.put("group.id", "helloconsumer");
+    props.put("enable.auto.commit", "true");
+    props.put("auto.commit.interval.ms", "1000");
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.LongDeserializer");
+    props.put("value.deserializer", "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+    props.put("schema.registry.url", "http://localhost:8081");
 
-		// consumer.close(); //unreachable code
-	}
+    HelloWorldConsumer helloWorldConsumer = new HelloWorldConsumer();
+    helloWorldConsumer.consume(props);
+
+    Runtime.getRuntime().addShutdownHook(new Thread(helloWorldConsumer::shutdown));
+  }
+
+  private void consume(Properties props) {
+    try (KafkaConsumer<Long, Alert> consumer = new KafkaConsumer<>(props)) {
+      consumer.subscribe(Collections.singletonList("avrotest"));
+
+      while (keepConsuming) {
+        ConsumerRecords<Long, Alert> records = consumer.poll(Duration.ofMillis(100));
+        for (ConsumerRecord<Long, Alert> record : records) {
+          log.info("[Consumer Record] offset = {}, key = {}, value = {}",
+                   record.offset(),
+                   record.key(),
+                   record.value());
+        }
+      }
+    }
+  }
+
+  private void shutdown() {
+    keepConsuming = false;
+  }
 
 }
