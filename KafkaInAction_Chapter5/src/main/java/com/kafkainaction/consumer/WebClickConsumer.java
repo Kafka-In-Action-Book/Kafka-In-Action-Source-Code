37f2254f233f6553bdec92108de0a5cc6cbd0c99
Dylan Scott <drrscott@hotmail.com>
2021-05-30 22:29:45 -0500

    Update WebClickConsumer.java

--- KafkaInAction_Chapter5/src/main/java/com/kafkainaction/consumer/WebClickConsumer.java
+++ KafkaInAction_Chapter5/src/main/java/com/kafkainaction/consumer/WebClickConsumer.java
@@ -4,6 +4,9 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
