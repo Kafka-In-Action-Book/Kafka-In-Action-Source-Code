4e5906fbdcd58cb103a2566ede29d4fd94c2a396
Dylan Scott <drrscott@hotmail.com>
2021-05-30 22:31:51 -0500

    Update HealthTrendingConsumer.java

--- KafkaInAction_Chapter5/src/main/java/com/kafkainaction/consumer/HealthTrendingConsumer.java
+++ KafkaInAction_Chapter5/src/main/java/com/kafkainaction/consumer/HealthTrendingConsumer.java
@@ -4,6 +4,9 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -11,6 +14,8 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import com.kafkainaction.model.Alert;
 
 public class HealthTrendingConsumer {
+	
+	final static Logger log = LoggerFactory.getLogger(HealthTrendingConsumer.class);
 
 	public static void main(String[] args) {
 		Properties props = new Properties();
@@ -28,7 +33,7 @@ public class HealthTrendingConsumer {
 		while (true) {
 			ConsumerRecords<Alert, String> records = consumer.poll(Duration.ofMillis(100));
 			for (ConsumerRecord<Alert, String> record : records) {
-				System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key().getStageId(), record.value());
+				log.info("offset = %d, key = %s, value = %s%n", record.offset(), record.key().getStageId(), record.value());
 			}
 		}
 
