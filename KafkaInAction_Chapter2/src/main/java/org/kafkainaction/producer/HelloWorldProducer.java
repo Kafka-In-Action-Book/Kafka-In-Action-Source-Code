c3104dbe664b55c2914a3de08255be45b7444e3b
Dylan Scott <drrscott@hotmail.com>
2021-06-02 11:54:24 -0500

    Update HelloWorldProducer.java

--- KafkaInAction_Chapter2/src/main/java/org/kafkainaction/producer/HelloWorldProducer.java
+++ KafkaInAction_Chapter2/src/main/java/org/kafkainaction/producer/HelloWorldProducer.java
@@ -11,13 +11,13 @@ public class HelloWorldProducer {
   public static void main(String[] args) {
 
     Properties props = new Properties();
-    props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+    props.put("bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094");
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-    try (Producer<String, String> producer = new KafkaProducer<>(props)) {
+    try (Producer<String, String> producer = new KafkaProducer<String, String>(props)) {
 
-      ProducerRecord<String, String> producerRecord = new ProducerRecord<>("helloworld", null,
+      ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("helloworld", null,
                                                                            "hello world again!");
       producer.send(producerRecord);
     }
