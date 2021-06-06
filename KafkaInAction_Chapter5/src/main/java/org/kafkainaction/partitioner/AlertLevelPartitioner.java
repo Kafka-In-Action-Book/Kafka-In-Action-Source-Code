ec38c929623120d38db98c8f9381943d44c3d85d
Dylan Scott <drrscott@hotmail.com>
2021-06-05 22:41:07 -0500

    update alert part

--- KafkaInAction_Chapter5/src/main/java/org/kafkainaction/partitioner/AlertLevelPartitioner.java
+++ KafkaInAction_Chapter5/src/main/java/org/kafkainaction/partitioner/AlertLevelPartitioner.java
@@ -1,5 +1,5 @@
 package org.kafkainaction.partitioner;
-import java.nio.charset.StandardCharsets;
+
 import java.util.List;
 import java.util.Map;
 
@@ -11,39 +11,26 @@ import org.kafkainaction.model.Alert;
 
 public class AlertLevelPartitioner implements Partitioner { // #A <1>
 
-    public int partition(final String topic,
-                         final Object objectKey,
-                         final byte[] keyBytes,
-                         final Object value,
-                         final byte[] valueBytes,
-                         final Cluster cluster) {
+	public int partition(final String topic, final Object objectKey, final byte[] keyBytes, final Object value,
+			final byte[] valueBytes, final Cluster cluster) {
 
-        final List<PartitionInfo> partitionInfoList =
-                cluster.availablePartitionsForTopic(topic);
-        final int partitionSize = partitionInfoList.size();
-        final int criticalPartition = partitionSize - 1;
-        final int partitionCount = partitionSize;
+		final List<PartitionInfo> partitionMetaList = cluster.availablePartitionsForTopic(topic);
+		final int partitionMod = partitionMetaList.size() - 1;
+		final int criticalPartition = 0;
 
-        final String alertLevel = new String(valueBytes, StandardCharsets.UTF_8);
+		final String key = ((Alert) objectKey).getAlertLevel();
 
-        if (alertLevel.equalsIgnoreCase("CRITICAL")) { // #B <2>
-            return criticalPartition;
-        } else {
-        	String stageId = ((Alert) objectKey).getStageId();
-            return Math.abs(stageId.hashCode()) % partitionCount;
-        }
-
-    }
+		return key.contains("CRITICAL") ? criticalPartition : Math.abs(key.hashCode()) % partitionMod;
+	}
 
 	public void configure(Map<String, ?> configs) {
 		// nothing needed
-		
+
 	}
 
 	public void close() {
 		// nothing needed
-		
-	}
 
+	}
 
 }
