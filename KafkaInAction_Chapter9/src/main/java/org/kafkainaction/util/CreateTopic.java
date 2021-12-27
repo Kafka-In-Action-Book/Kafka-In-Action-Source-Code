6202a612f1b1a326e2b304fc68c0dd3382f3ac62
Dylan Scott <drrscott@hotmail.com>
2021-12-27 11:42:44 -0600

    [actions skip]

--- KafkaInAction_Chapter9/src/main/java/org/kafkainaction/util/CreateTopic.java
+++ KafkaInAction_Chapter9/src/main/java/org/kafkainaction/util/CreateTopic.java
@@ -7,21 +7,22 @@ import org.apache.kafka.common.errors.TopicExistsException;
 
 import java.util.Collections;
 import java.util.Properties;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class CreateTopic {
 
   public static void main(String[] args) {
 
-    Properties props = new Properties();
-    props.put("bootstrap.servers", "localhost:9092,localhost:9093");
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    NewTopic requestedTopic = new NewTopic("selfserviceTopic", 2, (short) 2);
-    AdminClient client = AdminClient.create(props);
-    CreateTopicsResult topicResult = client.createTopics(Collections.singleton(requestedTopic));
+    Properties kaProperties = new Properties();
+    kaProperties.put("bootstrap.servers", "localhost:9092,localhost:9093");
+    kaProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    kaProperties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    NewTopic requestedTopic = new NewTopic("kinaction_selfserviceTopic", 2, (short) 2);
+    AdminClient client = AdminClient.create(kaProperties);
+    CreateTopicsResult topicResult = client.createTopics(List.of(requestedTopic));
     try {
-      topicResult.values().get("selfserviceTopic").get();
+      topicResult.values().get("kinaction_selfserviceTopic").get();
     } catch (InterruptedException e) {
       e.printStackTrace();
     } catch (ExecutionException e) {
