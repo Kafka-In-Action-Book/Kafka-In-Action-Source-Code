09e42d6ca14b2b37a52f30642b57c5935bb3707a
Dylan Scott <drrscott@hotmail.com>
2021-12-27 11:47:54 -0600

    [actions skip]

--- KafkaInAction_Chapter9/src/main/java/org/kafkainaction/consumer/AlertConsumer.java
+++ KafkaInAction_Chapter9/src/main/java/org/kafkainaction/consumer/AlertConsumer.java
@@ -14,22 +14,22 @@ public class AlertConsumer {
   final static Logger log = LoggerFactory.getLogger(AlertConsumer.class);
 
   public static void main(String[] args) {
-    Properties props = new Properties();
-    props.put("bootstrap.servers", "localhost:9092,localhost:9093");
-    props.put("group.id", "alertinterceptor");
-    props.put("enable.auto.commit", "true");
-    props.put("auto.commit.interval.ms", "1000");
-    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-    props.put("interceptor.classes", AlertConsumerMetricsInterceptor.class.getName());
+    Properties kaProperties = new Properties();
+    kaProperties.put("bootstrap.servers", "localhost:9092,localhost:9093");
+    kaProperties.put("group.id", "kinaction_alertinterceptor");
+    kaProperties.put("enable.auto.commit", "true");
+    kaProperties.put("auto.commit.interval.ms", "1000");
+    kaProperties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    kaProperties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    kaProperties.put("interceptor.classes", AlertConsumerMetricsInterceptor.class.getName());
 
-    try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+    try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(kaProperties)) {
       consumer.subscribe(List.of("kinaction_alert"));
 
       while (true) {
-        var records = consumer.poll(Duration.ofMillis(100));
+        var records = consumer.poll(Duration.ofMillis(250));
         for (ConsumerRecord<String, String> record : records) {
-          log.info("offset = {}, key = {}, value = {}", record.offset(), record.key(), record.value());
+          log.info("kinaction_info offset = {}, value = {}", record.offset(), record.value());
         }
       }
     }
