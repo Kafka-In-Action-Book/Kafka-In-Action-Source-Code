ebcfe759ab1227891e3a9c7afee8305c0db56b19
Dylan Scott <drrscott@hotmail.com>
2021-12-27 13:07:42 -0600

    [actions skip]

--- KafkaInAction_Chapter5/src/main/java/org/kafkainaction/consumer/AuditConsumer.java
+++ KafkaInAction_Chapter5/src/main/java/org/kafkainaction/consumer/AuditConsumer.java
@@ -19,37 +19,37 @@ public class AuditConsumer {
   private volatile boolean keepConsuming = true;
 
   public static void main(String[] args) {
-    Properties props = new Properties();
-    props.put("bootstrap.servers", "localhost:9092,localhost:9093");
-    props.put("enable.auto.commit", "false");
-    props.put("group.id", "audit");
-    props.put("key.deserializer",
+    Properties kaProperties = new Properties();
+    kaProperties.put("bootstrap.servers", "localhost:9092,localhost:9093");
+    kaProperties.put("enable.auto.commit", "false");
+    kaProperties.put("group.id", "kinaction_group_audit");
+    kaProperties.put("key.deserializer",
               "org.apache.kafka.common.serialization.StringDeserializer");
-    props.put("value.deserializer",
+    kaProperties.put("value.deserializer",
               "org.apache.kafka.common.serialization.StringDeserializer");
 
     final AuditConsumer auditConsumer = new AuditConsumer();
-    auditConsumer.consume(props);
+    auditConsumer.consume(kaProperties);
     Runtime.getRuntime().addShutdownHook(new Thread(auditConsumer::shutdown));
   }
 
-  private void consume(final Properties props) {
-    try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+  private void consume(final Properties kaProperties) {
+    try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(kaProperties)) {
 
       consumer.subscribe(List.of("kinaction_audit"));
 
       while (keepConsuming) {
-        var records = consumer.poll(Duration.ofMillis(100));
+        var records = consumer.poll(Duration.ofMillis(250));
         for (ConsumerRecord<String, String> record : records) {
-          log.info("offset = {}, key = {}, value = {}",
-                   record.offset(), record.key(), record.value());
+          log.info("kinaction_info offset = {}, value = {}",
+                   record.offset(), record.value());
 
-          OffsetAndMetadata offsetMeta = new OffsetAndMetadata(record.offset() + 1, "");
+          OffsetAndMetadata offsetMeta = new OffsetAndMetadata(++record.offset(), "");
 
-          Map<TopicPartition, OffsetAndMetadata> offsetMap = new HashMap<>();
-          offsetMap.put(new TopicPartition("audit", record.partition()), offsetMeta);
+          Map<TopicPartition, OffsetAndMetadata> kaOffsetMap = new HashMap<>();
+          kaOffsetMap.put(new TopicPartition("kinaction_audit", record.partition()), offsetMeta);
 
-          consumer.commitSync(offsetMap);
+          consumer.commitSync(kaOffsetMap);
         }
       }
     }
